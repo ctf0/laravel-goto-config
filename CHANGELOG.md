b3b2dc3d672a8199ee68dc2697a5022c37e36598
Muah <ctf0@users.noreply.github.com>
2019-05-12 04:09:47 +0200

    0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,6 @@
 
 ## 0.0.2
 - add support to mutli config folders
+
+## 0.0.3
+- add a way to stop the extension from search big files
--- src/providers/linkProvider.ts
+++ src/providers/linkProvider.ts
@@ -6,31 +6,38 @@ import {
     ProviderResult,
     DocumentLink,
     Position,
-    Range,
+    workspace,
+    Range
 } from "vscode"
 import * as util from '../util';
 
 export default class LinkProvider implements vsDocumentLinkProvider {
     public provideDocumentLinks(doc: TextDocument): ProviderResult<DocumentLink[]> {
+        let reg = /(?<=config\(|Config::get\()(['"])[^'"]*\1/g;
+        let config = workspace.getConfiguration('laravel_goto_config');
+        let linesCount = doc.lineCount
         let documentLinks = [];
         let index = 0;
-        let reg = /(?<=config\(|Config::get\()(['"])[^'"]*\1/g;
 
-        while (index < doc.lineCount) {
-            let line = doc.lineAt(index);
-            let result = line.text.match(reg);
-            if (result != null) {
-                for (let item of result) {
-                    let file = util.getFilePath(item, doc);
-                    if (file != null) {
-                        let start = new Position(line.lineNumber, line.text.indexOf(item));
-                        let end = start.translate(0, item.length);
-                        let documentlink = new DocumentLink(new Range(start, end), file.fileUri);
-                        documentLinks.push(documentlink);
-                    };
+        if (linesCount <= config.maxLinesCount) {
+            while (index < linesCount) {
+                let line = doc.lineAt(index);
+                let result = line.text.match(reg);
+
+                if (result != null) {
+                    for (let item of result) {
+                        let file = util.getFilePath(item, doc);
+                        if (file != null) {
+                            let start = new Position(line.lineNumber, line.text.indexOf(item));
+                            let end = start.translate(0, item.length);
+                            let documentlink = new DocumentLink(new Range(start, end), file.fileUri);
+                            documentLinks.push(documentlink);
+                        };
+                    }
                 }
+
+                index++;
             }
-            index++;
         }
 
         return documentLinks;
